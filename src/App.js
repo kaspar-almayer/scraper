40c7f502159612d375bbbd247ec14f05ecfe4baa
kaspar-almayer <kaspar_almayer@protonmail.com>
2022-07-26 21:19:24 +0200

    change title

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">scraper test</header>
+      <header className="App-header">scraper test 222</header>
     </div>
   );
 }
