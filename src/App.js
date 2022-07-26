e60a1ea99837f530678583bb28478e722fb34952
kaspar-almayer <kaspar_almayer@protonmail.com>
2022-07-26 12:10:47 +0200

    fix eslint

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        scraper test
-        
-              </header>
+      <header className="App-header">scraper test</header>
     </div>
   );
 }
